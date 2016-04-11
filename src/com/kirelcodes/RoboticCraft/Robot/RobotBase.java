8169f0920e4302a4870700d24bb8e16fe0915a33
NacOJerk <nacojerk@hotmail.com>
2016-04-11 15:14:05 +0300

    Robotic

---
+++ src/com/kirelcodes/RoboticCraft/Robot/RobotBase.java
@@ -0,0 +1,11 @@
+package com.kirelcodes.RoboticCraft.Robot;
+
+import org.bukkit.entity.ArmorStand;
+import org.bukkit.entity.Chicken;
+
+public class RobotBase {
+	private Chicken chick;
+	private ArmorStand armorStand;
+	private boolean isStuck;
+	private int ID;
+}
