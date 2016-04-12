2b13d17d1d3007c901b782d04d9f53c04fb83837
Gal Horowitz <galush.horowitz@gmail.com>
2016-04-12 22:51:57 +0300

    Revert "GAL FIX THE FXKING BAG"
    This reverts commit cd64a943865a17f49ce74e7b575ff84ba6da3516.

--- src/com/kirelcodes/RoboticCraft/pathFinders/MinerPathfinder.java
+++ src/com/kirelcodes/RoboticCraft/pathFinders/MinerPathfinder.java
@@ -11,7 +11,6 @@ public class MinerPathfinder extends BasicPathfinder {
 	private int blockIterator;
 	private int stairIterator;
 	private Location previus;
-
 	public MinerPathfinder(RobotMiner robot) {
 		this.robot = robot;
 		currentHeight = robot.getLocation().getBlockY();
@@ -23,24 +22,21 @@ public class MinerPathfinder extends BasicPathfinder {
 	public boolean shouldStart() {
 		return robot.isMining();
 	}
-
 	@Override
 	public void onStart() {
 		previus = robot.getStartBlock();
 	}
-
 	@Override
 	public void updateTask() {
 		if (robot.onDelay())
 			return;
-		if (blockIterator == 0) {
+		if (blockIterator == 0)
 			currentHeight--;
-			stairIterator++;
-		}
 		if (currentHeight == 1) {
 			robot.setMining(false);
 		}
 		blockIterator++;
+		stairIterator++;
 		Location mine = robot.getStartBlock().clone();
 		mine.setY(currentHeight);
 		try {
@@ -49,54 +45,48 @@ public class MinerPathfinder extends BasicPathfinder {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		if(blockIterator == 1 && stairIterator == 1){
-			previus = mine;
-			return;
-		}
 		switch (blockIterator) {
 		case 1:
-			if (stairIterator != 1)
+			if(stairIterator!=1)
 				mine.add(0, 0, 0);
-			else
-				mine.add(0, 1, 0);
 			break;
 		case 2:
-			if (stairIterator != 2)
+			if(stairIterator!=2)
 				mine.add(1, 0, 0);
 			break;
 		case 3:
-			if (stairIterator != 3)
+			if(stairIterator!=3)
 				mine.add(2, 0, 0);
 			break;
 		case 4:
-			if (stairIterator != 8)
+			if(stairIterator!=8)
 				mine.add(0, 0, 1);
-			if (stairIterator == 8)
-				stairIterator = 1;
 			break;
 		case 5:
 			mine.add(1, 0, 1);
 			break;
 		case 6:
-			if (stairIterator != 4)
+			if(stairIterator!=4)
 				mine.add(2, 0, 1);
 			break;
 		case 7:
-			if (stairIterator != 7)
+			if(stairIterator!=7)
 				mine.add(0, 0, 2);
 			break;
 		case 8:
-			if (stairIterator != 6)
+			if(stairIterator!=6)
 				mine.add(1, 0, 2);
 			break;
 		case 9:
-			if (stairIterator != 5)
+			if(stairIterator!=5)
 				mine.add(2, 0, 2);
 			blockIterator = 0;
 			break;
 		}
 		robot.mineBlock(mine);
 		previus = mine;
+		if(stairIterator==8)
+			stairIterator=0;
 	}
 
 }
