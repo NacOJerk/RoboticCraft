3349d43bf472375c07fe99fb6b91058a40d0fa4a
NacOJerk <nacojerk@hotmail.com>
2016-04-13 18:28:27 +0300

    hey

--- src/com/kirelcodes/RoboticCraft/listener/RobotListener.java
+++ src/com/kirelcodes/RoboticCraft/listener/RobotListener.java
@@ -1,11 +1,31 @@
 package com.kirelcodes.RoboticCraft.listener;
 
+import net.md_5.bungee.api.ChatColor;
+
 import org.bukkit.Bukkit;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerArmorStandManipulateEvent;
+import org.bukkit.inventory.Inventory;
 import org.bukkit.plugin.Plugin;
+
+import com.kirelcodes.RoboticCraft.robot.RobotCenter;
 
 public class RobotListener implements Listener{
 	public RobotListener(Plugin p){
 		Bukkit.getPluginManager().registerEvents(this, p);
+	}
+	@EventHandler
+	public void intaractAtArmor(PlayerArmorStandManipulateEvent e){
+		
+		System.out.println(e.getRightClicked().getCustomName());
+		if(!e.getRightClicked().getCustomName().contains(ChatColor.MAGIC + "NacOJerkGalShaked-"))
+			return;
+		e.setCancelled(true);
+		int ID = Integer.parseInt(e.getRightClicked().getCustomName().split("-")[1]);
+		if(!RobotCenter.exists(ID))
+			return;
+		Inventory inven = RobotCenter.getRobot(ID).getInventory();
+		e.getPlayer().openInventory(inven);
 	}
 }
