a82d9b7d20f0f9ea3949f71a9c7c7ff84144639a
Voigon <liorslakman3@gmail.com>
2016-04-16 12:47:12 +0300

    RobotHunterGUI

---
+++ src/com/kirelcodes/RoboticCraft/gui/guiRobots/GUIRobotHunter.java
@@ -0,0 +1,110 @@
+package com.kirelcodes.RoboticCraft.gui.guiRobots;
+
+import java.util.Random;
+
+import org.bukkit.Material;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+
+import com.kirelcodes.RoboticCraft.gui.GUI;
+import com.kirelcodes.RoboticCraft.robot.RobotHunter;
+import com.kirelcodes.RoboticCraft.utils.ItemStackUtils;
+
+import net.md_5.bungee.api.ChatColor;
+
+public class GUIRobotHunter extends GUI{
+	private RobotHunter robot;
+	private ItemStack itemFollow, itemNoFollow, itemHunt, itemNoHunt, openInventory;
+
+	public RobotHunter getRobot(){
+		return robot;
+	}
+	public GUIRobotHunter(RobotHunter robot) {
+		setSize(27);
+		setTitle("&cLumberjack Robot GUI");
+		instalizeInventory();
+		this.robot = robot;
+		itemFollow = ItemStackUtils.createItem(Material.COMPASS, "&aFollow");
+		itemNoFollow = ItemStackUtils.createItem(Material.COMPASS, "&cStop Follow");
+		itemHunt = ItemStackUtils.createItem(Material.DIAMOND_SWORD, "&aHunt");
+		itemNoHunt = ItemStackUtils.createItem(Material.DIAMOND_SWORD, "&cStop Hunt");
+		openInventory = ItemStackUtils.createItem(Material.CHEST, "&cOpen Robot's Inventory");
+		gettGUIAction().add(new GUIAction(itemFollow) {
+
+			@Override
+			public void actionNow(GUI gui, Player player) {
+				((GUIRobotHunter) gui).follow(player);
+			}
+		});
+		gettGUIAction().add(new GUIAction(openInventory) {
+
+			@Override
+			public void actionNow(GUI gui, Player player) {
+				player.closeInventory();
+				player.openInventory(((GUIRobotHunter)gui).getRobot().getInventory());
+			}
+		});
+		gettGUIAction().add(new GUIAction(itemNoFollow) {
+
+			@Override
+			public void actionNow(GUI gui, Player player) {
+				((GUIRobotHunter) gui).noFollow(player);
+			}
+		});
+		gettGUIAction().add(new GUIAction(itemHunt) {
+
+			@Override
+			public void actionNow(GUI gui, Player player) {
+				((GUIRobotHunter) gui).Hunt();
+			}
+		});
+		gettGUIAction().add(new GUIAction(itemNoHunt) {
+
+			@Override
+			public void actionNow(GUI gui, Player player) {
+				((GUIRobotHunter) gui).noHunt();
+			}
+		});
+		for (int i = 0; i < 27; i++) {
+			ItemStack item = ItemStackUtils.createItem(Material.STAINED_GLASS_PANE, new Random().nextInt(16),
+					ChatColor.BLACK + "DONT CLICK ME");
+			getInventory().setItem(i, item);
+			gettGUIAction().add(new GUIAction(item) {
+
+				@Override
+				public void actionNow(GUI gui, Player player) {
+
+				}
+			});
+		}
+		getInventory().setItem(13, openInventory);
+		getInventory().setItem(14, (robot.isHunting()) ? itemNoHunt : itemHunt);
+		getInventory().setItem(12, (robot.isFollowing()) ? itemNoFollow : itemFollow);
+	}
+
+	public void follow(Entity p) {
+		robot.setFollow(p);
+		if(robot.isHunting())
+			noHunt();
+		getInventory().setItem(12, itemNoFollow);
+	}
+
+	public void noFollow(Entity p) {
+		robot.cancelFollow();
+		getInventory().setItem(12, itemFollow);
+	}
+
+	public void Hunt() {
+		robot.setHunting(true);
+		if(robot.isFollowing())
+			noFollow(robot.getFollowTarget());
+		getInventory().setItem(14, itemNoHunt);
+	}
+
+	public void noHunt() {
+		robot.setHunting(false);
+		getInventory().setItem(14, itemHunt);
+	}
+
+}
