a4ba87a87a32ce0bcd6aad4b73d5977e4d2513f1
xShakedDev <xShakedDev@gmail.com>
2016-04-20 21:53:36 +0300

    Martin

--- src/com/kirelcodes/RoboticCraft/pathFinders/FisherPathfinder.java
+++ src/com/kirelcodes/RoboticCraft/pathFinders/FisherPathfinder.java
@@ -15,6 +15,7 @@ public class FisherPathfinder extends BasicPathfinder {
 	private Block target;
 	private int clock;
 	private double time = ConfigManager.getFishTime();
+
 	public FisherPathfinder(RobotFisher robot) {
 		this.robot = robot;
 	}
@@ -35,8 +36,7 @@ public class FisherPathfinder extends BasicPathfinder {
 		clock++;
 		if (target == null)
 			return;
-		if (target.getType() == Material.AIR
-				|| (target.getY() - robot.getLocation().getY()) > 5)
+		if (target.getType() == Material.AIR || (target.getY() - robot.getLocation().getY()) > 5)
 			target = null;
 		if (target == null || robot == null)
 			return;
@@ -44,22 +44,21 @@ public class FisherPathfinder extends BasicPathfinder {
 
 	@Override
 	public void updateTask() {
-		int radius = 20;
+		int radius = 15;
 		ArrayList<Block> water = new ArrayList<Block>();
 		if (target == null) {
 			int x = robot.getLocation().getBlockX();
 			int z = robot.getLocation().getBlockZ();
 			for (int y = radius; y > -radius; y--) {
-				water.addAll(robot.getNearbyBlocks(robot.getWorld().getBlockAt(x, robot.getLocation().getBlockY() + y, z).getLocation(), radius));
+				water.addAll(robot.getNearbyBlocks(
+						robot.getWorld().getBlockAt(x, robot.getLocation().getBlockY() + y, z).getLocation(), radius));
 			}
 		}
 		for (Block bs : water) {
 			if (bs == null)
 				continue;
-			if (bs.getType() == Material.WATER
-					|| bs.getType() == Material.STATIONARY_WATER) {
+			if (bs.getType() == Material.WATER || bs.getType() == Material.STATIONARY_WATER) {
 				target = bs;
-				//Bukkit.broadcastMessage("" + target.getType());
 				try {
 					robot.setTargetLocation(bs.getLocation());
 				} catch (Exception e) {
@@ -70,19 +69,19 @@ public class FisherPathfinder extends BasicPathfinder {
 		}
 		if (target == null)
 			return;
-		if(!(target.getType() == Material.WATER || target.getType() == Material.STATIONARY_WATER))
+		if (!(target.getType() == Material.WATER || target.getType() == Material.STATIONARY_WATER))
 			return;
 		try {
 			robot.setTargetLocation(target.getLocation());
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		if(target.getLocation().distance(robot.getLocation()) > 4)
+		if (target.getLocation().distance(robot.getLocation()) > 4)
 			return;
 		if ((clock % time) != 0)
 			return;
 		Random rand = new Random();
-		short s = (short) rand.nextInt(3);
+		short s = (short) rand.nextInt(4);
 		ItemStack fish = new ItemStack(Material.RAW_FISH, 1, s);
 		robot.getInventory().addItem(fish);
 		clock = 0;
